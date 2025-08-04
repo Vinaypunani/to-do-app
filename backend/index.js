17762a0e0573c94e0353f2418859264e12fed676
Vinaypunani <vinaypunani@gmail.com>
2025-08-04 13:19:56 +0530

    backend setup ready

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,11 @@
-// Entry point for the backend app
+const express = require("express");
+require('dotenv').config();
+
+
+const app = express();
+const PORT = process.env.PORT
+app.get('/',(req,res)=>{
+    res.send("hello world!")
+})
+
+app.listen(PORT,()=>{console.log(`Server Running on the PORT:${PORT}`)})
