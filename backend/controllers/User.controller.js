4aa6506475cd61d0a29c71a69a00ba83cccadd42
Omprakash prajapat <omprakashprjpt118@gmail.com>
2025-08-05 21:56:37 +0530

    implement user controller with CRUD operations; add ApiError and ApiResponse utilities; remove sample controller and helper

---
+++ backend/controllers/User.controller.js
@@ -0,0 +1,25 @@
+const asyncHandler = require('../utils/asyncHandler');
+const ApiError = require('../utils/ApiError');
+const ApiResponse = require('../utils/ApiResponse');
+
+
+const getAllUser = asyncHandler(async (req,res) => {
+
+})
+const getUserById = asyncHandler(async (req,res) => {
+
+})
+const updateUserData = asyncHandler(async (req,res) => {
+
+})
+const deleteUserData = asyncHandler(async (req,res) => {
+
+})
+
+
+module.exports = {
+    getAllUser,
+    getUserById,
+    updateUserData,
+    deleteUserData
+}
